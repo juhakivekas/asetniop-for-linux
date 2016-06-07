2d204c65e20f8cc4f6b20d573b529432d5efdfa0
juhakivekas <juha.kivekas@helsinki.com>
2016-06-07 19:36:33 +0300

    minor cleanup and simplification

--- src/asetniop.c
+++ src/asetniop.c
@@ -18,8 +18,7 @@
 
 static int forward_event(struct libevdev_uinput *uidev, struct input_event *ev, struct chord* state)
 {
-	int err;
-
+	int ret;
 	enum touch t = NUM_TOUCHES;
 	switch(ev->code){
 	case KEY_A:         t = TOUCH_A; break;
@@ -42,54 +41,50 @@ static int forward_event(struct libevdev_uinput *uidev, struct input_event *ev, 
 			int keycode = lookup_keycode(k);
 			if(keycode != KEY_RESERVED){
 				fprintf(stderr, "chord 0x%02x %s\n", k, libevdev_event_code_get_name(EV_KEY, keycode));
-				err = libevdev_uinput_write_event(uidev, EV_KEY, keycode   , 1);
-				if (err < 0) {perror("Failed to write event"); return -1;}
-				err = libevdev_uinput_write_event(uidev, EV_KEY, keycode   , 0);
-				if (err < 0) {perror("Failed to write event"); return -1;}
-				err = libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-				if (err < 0) {perror("Failed to write event"); return -1;}
+				ret = libevdev_uinput_write_event(uidev, EV_KEY, keycode   , 1);
+				if (ret < 0) {perror("Failed to write event"); return -1;}
+				ret = libevdev_uinput_write_event(uidev, EV_KEY, keycode   , 0);
+				if (ret < 0) {perror("Failed to write event"); return -1;}
+				ret = libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
+				if (ret < 0) {perror("Failed to write event"); return -1;}
 			}
 			chord_reset(state);
 		}
 	} else {
-//	printf("%s, %s, %d\n",
-//		libevdev_event_type_get_name(ev->type),
-//		libevdev_event_code_get_name(ev->type, ev->code),
-//		ev->value);
+//		printf("%s, %s, %d\n",
+//			libevdev_event_type_get_name(ev->type),
+//			libevdev_event_code_get_name(ev->type, ev->code),
+//			ev->value);
 
-		err = libevdev_uinput_write_event(uidev, EV_KEY, ev->code  , ev->value);
-		if (err < 0) {perror("Failed to write event"); return -1;}
-		err = libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
-		if (err < 0) {perror("Failed to write event"); return -1;}
+		ret = libevdev_uinput_write_event(uidev, EV_KEY, ev->code  , ev->value);
+		if (ret < 0) {perror("Failed to write event"); return -1;}
+		ret = libevdev_uinput_write_event(uidev, EV_SYN, SYN_REPORT, 0);
+		if (ret < 0) {perror("Failed to write event"); return -1;}
 	}
 	return 0;
 }
 
-int intercept(
-		struct libevdev_uinput *uidev,
-		struct libevdev *dev){
-	int err;
-	//initialize some objects
+int intercept(struct libevdev_uinput *uidev, struct libevdev *dev){
+	int ret;
+	struct input_event ev;
 	struct chord state;
 	chord_reset(&state);
 	lookup_init("dat/test-keymap-minimal.dat");
-	do {
-		struct input_event ev;
-		err = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL|LIBEVDEV_READ_FLAG_BLOCKING, &ev);
-		if (err == LIBEVDEV_READ_STATUS_SYNC) {
-		//the status was dropped, re-sync
-			printf("dropped, syncing status\n");
-			while (err == LIBEVDEV_READ_STATUS_SYNC) {
-				err = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_SYNC, &ev);
-			}
-		} else if (err == LIBEVDEV_READ_STATUS_SUCCESS){
-			if(ev.type == EV_KEY && ev.code == KEY_ESC) return 0; //kill the interceptor with the escape key!
-			if(ev.type == EV_KEY && ev.value != 2) forward_event(uidev, &ev, &state);
-		}
-	} while (err == LIBEVDEV_READ_STATUS_SYNC || err == LIBEVDEV_READ_STATUS_SUCCESS || err == -EAGAIN);
 
-	if (err != LIBEVDEV_READ_STATUS_SUCCESS && err != -EAGAIN)
-		fprintf(stderr, "Failed to handle events: %s\n", strerror(-err));
+	do {
+		ret = libevdev_next_event(dev, LIBEVDEV_READ_FLAG_NORMAL|LIBEVDEV_READ_FLAG_BLOCKING, &ev);
+		if (ret == LIBEVDEV_READ_STATUS_SUCCESS){
+			//kill the interceptor with the escape key!
+			if(ev.type == EV_KEY && ev.code == KEY_ESC)
+				return 0;
+			//forward key down and key up events
+			if(ev.type == EV_KEY && (ev.value == 1 || ev.value == 0))
+				forward_event(uidev, &ev, &state);
+		}
+	} while (ret == LIBEVDEV_READ_STATUS_SYNC || ret == LIBEVDEV_READ_STATUS_SUCCESS || ret == -EAGAIN);
+
+	if (ret != LIBEVDEV_READ_STATUS_SUCCESS && ret != -EAGAIN)
+		fprintf(stderr, "Failed to handle events: %s\n", strerror(-ret));
 
 	return 0;}
 
@@ -97,7 +92,7 @@ int main(int argc, char **argv)
 {
 	struct libevdev *dev = NULL;
 	struct libevdev_uinput *uidev = NULL;
-	int fd, err;
+	int fd, ret;
 
 	if (argc < 2){
 		fprintf(stderr, "Usage: %s <event device>\n", argv[0]);
@@ -109,21 +104,21 @@ int main(int argc, char **argv)
 	if (fd < 0) {perror("Failed to open device"); return fd;}
 
 	//create a libevdev device from the event file descriptor
-	err = libevdev_new_from_fd(fd, &dev);
-	if (err < 0) {perror("Failed to create libevdev device"); return err;}
+	ret = libevdev_new_from_fd(fd, &dev);
+	if (ret < 0) {perror("Failed to create libevdev device"); return ret;}
 
 	//create a device connected to the virtual device interface /dev/uinput
-	err = libevdev_uinput_create_from_device(
+	ret = libevdev_uinput_create_from_device(
 		dev,
 		LIBEVDEV_UINPUT_OPEN_MANAGED,
 		&uidev);
-	if (err < 0) {perror("Failed to create uinput device"); return err;}
+	if (ret < 0) {perror("Failed to create uinput device"); return ret;}
 
 	sleep(1);
 	//EVIOCGRAB the event, this means no other client will receive the events from the device, including your window manager.
 	// "This is generally a bad idea. Don't do this." - libevdev documentation
 	//We need to do this to avoid douplicate keyboard events.
-	err = libevdev_grab(dev, LIBEVDEV_GRAB);
+	ret = libevdev_grab(dev, LIBEVDEV_GRAB);
 
 	//start the interceptor and never return
 	intercept(uidev, dev);
@@ -131,5 +126,5 @@ int main(int argc, char **argv)
 	libevdev_uinput_destroy(uidev);
 	libevdev_grab(dev, LIBEVDEV_UNGRAB);
 	libevdev_free(dev);
-	return err;
+	return ret;
 }
